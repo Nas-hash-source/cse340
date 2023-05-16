a66d2725fe1331a8d1701781bec6d4473a84b156
Nasandratra Harivonjy <nasandratra.access4@gmail.com>
2023-05-16 04:55:24 -0700

    first deploy, backend development

--- server.js
+++ server.js
@@ -5,14 +5,35 @@
 /* ***********************
  * Require Statements
  *************************/
+const baseController = require("./controllers/baseController.js")
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
+
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root         
 
 /* ***********************
  * Routes
  *************************/
 app.use(require("./routes/static"))
+
+// Inventory routes
+app.use("/inv", require("./routes/inventoryRoute"))
+
+// Index route
+app.get("/", baseController.buildHome)
+app.get("/", function (req, res){
+  res.render("index", {title: "Home"})
+});
+
+
 
 /* ***********************
  * Local Server Information
